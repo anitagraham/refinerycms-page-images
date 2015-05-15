094cde722fa2525e6d4161d2a0f60adc1949133c
Anita Graham <anita@joli.com.au>
2015-05-15 20:26:27 +0800

    Get it right at last.

--- config/initializers/refinery/assets.rb
+++ config/initializers/refinery/assets.rb
@@ -1,8 +1,8 @@
 Refinery::Core.configure do |config|
   # Register extra javascript for backend
-  config.register_javascript "page-image-picker"
+  config.register_javascript "refinery/page-image-picker"
 
   # Register extra stylesheet for backend (optional options)
-  config.register_stylesheet "page-image-picker", :media => 'screen'
+  config.register_stylesheet "refinery/page-image-picker", :media => 'screen'
 end
 Rails.application.config.assets.precompile += %w( page-image-picker.css page-image-picker.js )
