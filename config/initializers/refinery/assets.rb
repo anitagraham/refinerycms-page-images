58bc740db70da511db21d5b111f58d3a05dc36c4
Anita Graham <anita@joli.com.au>
2015-05-15 19:47:13 +0800

    retry assets definition

--- config/initializers/refinery/assets.rb
+++ config/initializers/refinery/assets.rb
@@ -1,6 +1,6 @@
 Refinery::Core.configure do |config|
   # Register extra javascript for backend
-  config.register_javascript "page-image-picker.js"
+  config.register_javascript "page-image-picker"
 
   # Register extra stylesheet for backend (optional options)
   config.register_stylesheet "page-image-picker", :media => 'screen'
